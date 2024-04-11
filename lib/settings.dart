0bcacb9728e7bfea9a3dc7cb3be81425fc07255b
Nabinta1 <nni002@latech.edu>
2024-04-11 17:50:03 -0500

    Privacy and Logout setting

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,3 +1,4 @@
+import 'package:exercise_app/loginScreen.dart';
 import 'package:flutter/material.dart';
 import 'profile_settings.dart';
 import 'account_settings.dart';
@@ -16,13 +17,23 @@ class Settings extends StatelessWidget {
           ListTile(
             title: Text('Profile Settings'),
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => ProfileSettingsPage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ProfileSettingsPage(),
+                ),
+              );
             },
           ),
           ListTile(
             title: Text('Account Settings'),
             onTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => AccountSettingsPage()));
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AccountSettingsPage(),
+                ),
+              );
             },
           ),
           ListTile(
@@ -34,7 +45,47 @@ class Settings extends StatelessWidget {
           ListTile(
             title: Text('Privacy Settings'),
             onTap: () {
-              // Navigate to privacy settings page
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  bool isPrivate = false; // Default value for the switch
+                  return StatefulBuilder(
+                    builder: (context, setState) {
+                      return AlertDialog(
+                        title: Text('Privacy Settings'),
+                        content: SingleChildScrollView(
+                          // If content doesn't fit the screen
+                          child: ListBody(
+                            children: <Widget>[
+                              Text(
+                                "When your account is private, only the followers you approve can see what you share and your followers and following lists.",
+                              ),
+                              SwitchListTile(
+                                title: Text('Private Account'),
+                                value: isPrivate,
+                                onChanged: (bool value) {
+                                  setState(() {
+                                    isPrivate = value;
+                                  });
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: <Widget>[
+                          TextButton(
+                            child: Text('Done'),
+                            onPressed: () {
+                              Navigator.of(context).pop(); // Dismiss the dialog
+                              // Here you could also save the preference if needed
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              );
             },
           ),
           ListTile(
@@ -46,7 +97,35 @@ class Settings extends StatelessWidget {
           ListTile(
             title: Text('Log Out'),
             onTap: () {
-              // Implement log out functionality
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Text('Log Out'),
+                    content: Text('Are you sure you want to log out?'),
+                    actions: <Widget>[
+                      TextButton(
+                        child: Text('No'),
+                        onPressed: () {
+                          Navigator.of(context).pop(); // Dismissing the dialog
+                        },
+                      ),
+                      TextButton(
+                        child: Text('Yes'),
+                        onPressed: () {
+                          Navigator.of(context).pop(); //Dismissing the dialog
+                          Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => loginScreen()),
+                            ModalRoute.withName('/login'),
+                          );
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
             },
           ),
         ],
