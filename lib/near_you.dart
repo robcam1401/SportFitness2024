ab6950b07e40b4ecc53a24d8d61de5d8cd3cdfd6
robcam1401 <114763584+robcam1401@users.noreply.github.com>
2024-05-05 17:14:24 -0500

    Add files via upload

--- lib/near_you.dart
+++ lib/near_you.dart
@@ -1,78 +1,234 @@
+import 'dart:async';
+
+import 'package:exercise_app/feed.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:exercise_app/friends.dart';
 import 'package:exercise_app/groups.dart';
 
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-const LatLng sourceLocation = LatLng(32.53021599903092, -92.65212084047921);
-
-
-class NearYou extends StatefulWidget{
+class NearYou extends StatefulWidget {
   @override
-  State<NearYou> createState() => _NearYou();
+  NearYouState createState() => NearYouState();
 }
 
-class _NearYou extends State<NearYou> {
-List<Marker> myMarker = [];
+class NearYouState extends State<NearYou> {
+  Completer<GoogleMapController> _controller = Completer();
 
   @override
-  // created the google maps widget inside the column so it 
-  //would occupy the full space
+  void initState() {
+    super.initState();
+  }
+    double zoomVal=5.0;
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  const Text('Near You'),
-        centerTitle: true,
-        backgroundColor: Colors.red[600],
+        backgroundColor: Colors.red,
+        title: Text("Near You"),
+
       ),
-      body:  Column(
-        children: [
-           Expanded(
-            flex: 90,
-            child: GoogleMap(
-              initialCameraPosition: CameraPosition(target: sourceLocation, zoom: 14),
-              markers: Set.from(myMarker),
-              mapType: MapType.normal,
-              onTap: _handleTap,
-              ),
-          ),
-                //made a temporary row of two button to show the friends and groups list 
-                // pressing the button will switch to those pages
-                Row(
-                  children: [
-
-                    Expanded(
-                      child: Container(
-                        padding: const EdgeInsets.all(20),
-                        color: Colors.green,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text("group list"),
-                        ),
-                      ),
-                    ),
-
-                  ],
-                )
-        ]
-              ),
-          
-          );
+      body: Stack(
+        children: <Widget>[
+          _buildGoogleMap(context),
+          _zoomminusfunction(),
+          _zoomplusfunction(),
+          _buildContainer(),
+        ],
+      ),
+    );
   }
-          _handleTap(LatLng tappedPoint) {
-            print(tappedPoint);
-            setState(() {
-              myMarker = [];
-              myMarker.add(Marker(
-                markerId: MarkerId(tappedPoint.toString()),
-                position: tappedPoint,
-                draggable: true,
-                onDragEnd: (DragEndDetails) {
-                  print(DragEndDetails);
-                }
-              ));
-            });
-          }
-      
 
+ Widget _zoomminusfunction() {
+
+    return Align(
+      alignment: Alignment.topLeft,
+      child: IconButton(
+            icon: Icon(Icons.search,color:Color(0xff6200ee)),
+            onPressed: () {
+              zoomVal--;
+             _minus( zoomVal);
+            }),
+    );
+ }
+ Widget _zoomplusfunction() {
+   
+    return Align(
+      alignment: Alignment.topRight,
+      child: IconButton(
+            icon: Icon(Icons.search,color:Color(0xff6200ee)),
+            onPressed: () {
+              zoomVal++;
+              _plus(zoomVal);
+            }),
+    );
+ }
+
+ Future<void> _minus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  }
+  Future<void> _plus(double zoomVal) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(40.712776, -74.005974), zoom: zoomVal)));
+  }
+
+  
+  Widget _buildContainer() {
+    return Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 20.0),
+        height: 150.0,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSlmAd2R0GFoEGTJgpYGRrZQa5SWURczrDUtg&s",
+                  32.53070986780426, -92.65166205089903,"5k Run"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/fitness-competition-design-template-25dc8113019ab96a17c6d07d771e66bc_screen.jpg?ts=1637013383",
+                  32.533464322282775, -92.65099924429194,"Fitness Competition"),
+            ),
+            SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/event-poster-fitness-club-design-template-244dfd5ed622161bf5d1df0a7520a875_screen.jpg?ts=1645433908",
+                  32.5057384467211, -92.63482201512295,"Fitness Club"),
+            ),
+             SizedBox(width: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: _boxes(
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGKyf4nB-VLwNbE79ySmR6UHlNG6m8awwg0Q&s",
+                  32.545690522365746, -92.61870027903757,"Power Lifting Club"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _boxes(String _image, double lat,double long,String eventName) {
+    return  GestureDetector(
+        onTap: () {
+          _gotoLocation(lat,long);
+        },
+        child:Container(
+              child: FittedBox(
+                child: Material(
+                    color: Colors.white,
+                    elevation: 14.0,
+                    borderRadius: BorderRadius.circular(24.0),
+                    shadowColor: Color(0x802196F3),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Container(
+                          width: 180,
+                          height: 200,
+                          child: ClipRRect(
+                            borderRadius: new BorderRadius.circular(24.0),
+                            child: Image.network(_image)
+                          ),),
+                          Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: myDetailsContainer1(eventName),
+                          ),
+                        ),
+
+                      ],)
+                ),
+              ),
+            ),
+    );
+  }
+
+  Widget myDetailsContainer1(String eventName) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(left: 8.0),
+          child: Container(
+              child: Text(eventName,
+            style: TextStyle(
+                color: Color(0xff6200ee),
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold),
+          )),
+        ),
+      ]
+    );
+  }
+
+  Widget _buildGoogleMap(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition:  CameraPosition(target: sourceLocation, zoom: 12),
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        markers: {
+          gramercyMarker,bernardinMarker,blueMarker,powerMarker
+        },
+      ),
+    );
+  }
+
+  Future<void> _gotoLocation(double lat,double long) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(target: LatLng(lat, long), zoom: 15,
+      bearing: 45.0,)));
+  }
 }
+
+Marker gramercyMarker = Marker(
+  markerId: MarkerId('5k Run'),
+  position: LatLng(32.53070986780426, -92.65166205089903),
+  infoWindow: InfoWindow(title: '5K run'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+
+Marker bernardinMarker = Marker(
+  markerId: MarkerId('Fitness Competition'),
+  position: LatLng(32.533464322282775, -92.65099924429194),
+  infoWindow: InfoWindow(title: 'Fitness Competition'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+Marker blueMarker = Marker(
+  markerId: MarkerId('Fitness Club'),
+  position: LatLng(32.5057384467211, -92.63482201512295),
+  infoWindow: InfoWindow(title: 'Fitness CLub'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+
+Marker powerMarker = Marker(
+  markerId: MarkerId('Power Lifting Club'),
+  position: LatLng(32.545690522365746, -92.61870027903757),
+  infoWindow: InfoWindow(title: 'Power Lifting CLub'),
+  icon: BitmapDescriptor.defaultMarkerWithHue(
+    BitmapDescriptor.hueRed,
+  ),
+);
+
+
